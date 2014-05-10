e18e1c2e1d0d94181ba8c09a8040a6c5e3536649
FichteFoll <fichtefoll2@googlemail.com>
2014-05-10 03:04:07 +0200

    Run coder formatter on all files, especially with:
    - max line length 100
    - 4 space indents

--- src/main/java/example/ListProductRenderer.java
+++ src/main/java/example/ListProductRenderer.java
@@ -12,29 +12,30 @@ import javax.swing.ListCellRenderer;
 
 import fpt.com.Product;
 
+
 public class ListProductRenderer implements ListCellRenderer<Product> {
-	// verwendbar mit JList<Product>.setCellRenderer(new ListProductRenderer());
-	// methode
+    // verwendbar mit JList<Product>.setCellRenderer(new ListProductRenderer());
+    // methode
 
-	public Component getListCellRendererComponent(
-			JList<? extends Product> list, Product value, int index,
-			boolean isSelected, boolean cellHasFocus) {
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Product> list, Product value,
+            int index, boolean isSelected, boolean cellHasFocus) {
 
-		String namex = value.getName();
-		Box box = Box.createVerticalBox();
-		JLabel l = new JLabel(namex);
-		JLabel p = new JLabel("Price:" + value.getPrice());
-		JLabel q = new JLabel("Quantity:" + value.getQuantity());
-		Font f = l.getFont();
-		f = f.deriveFont(Font.ITALIC, f.getSize() * 0.8f);
-		p.setFont(f);
-		q.setFont(f);
-		box.add(l);
-		box.add(p);
-		box.add(q);
-		if (isSelected) {
-			box.setBorder(BorderFactory.createLineBorder(Color.blue));
-		}
-		return box;
-	}
+        String namex = value.getName();
+        Box box = Box.createVerticalBox();
+        JLabel l = new JLabel(namex);
+        JLabel p = new JLabel("Price:" + value.getPrice());
+        JLabel q = new JLabel("Quantity:" + value.getQuantity());
+        Font f = l.getFont();
+        f = f.deriveFont(Font.ITALIC, f.getSize() * 0.8f);
+        p.setFont(f);
+        q.setFont(f);
+        box.add(l);
+        box.add(p);
+        box.add(q);
+        if (isSelected) {
+            box.setBorder(BorderFactory.createLineBorder(Color.blue));
+        }
+        return box;
+    }
 }
--- src/main/java/fpt/com/JDBCInterface.java
+++ src/main/java/fpt/com/JDBCInterface.java
@@ -3,23 +3,23 @@ package fpt.com;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+
 public interface JDBCInterface {
-	void insert(Product product) throws SQLException;
+    void insert(Product product) throws SQLException;
 
-	long insert(String name, double price, int quantity) throws SQLException;
+    long insert(String name, double price, int quantity) throws SQLException;
 
-	Product read(long productId) throws SQLException;
+    Product read(long productId) throws SQLException;
 
-	ProductList findByName(String name) throws SQLException;
+    ProductList findByName(String name) throws SQLException;
 
-	ProductList findByQuantity(int quantity) throws SQLException;
+    ProductList findByQuantity(int quantity) throws SQLException;
 
-	ProductList find(String name, double price, int quantity)
-			throws SQLException;
+    ProductList find(String name, double price, int quantity) throws SQLException;
 
-	Connection getConnection();
+    Connection getConnection();
 
-	void setConnection(Connection connection);
+    void setConnection(Connection connection);
 
-	void dumpMetaData() throws SQLException;
+    void dumpMetaData() throws SQLException;
 }
