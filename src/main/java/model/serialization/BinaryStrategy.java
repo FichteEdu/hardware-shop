b957d621e0155304d129fb88488a0ec6ac0909e9
Robbilie <git@eneticum.de>
2014-05-14 10:48:49 +0200

    auto added methods to serialization strategies

--- src/main/java/model/serialization/BinaryStrategy.java
+++ src/main/java/model/serialization/BinaryStrategy.java
@@ -1,3 +1,27 @@
+package model.serialization;
+
+import java.io.IOException;
+
+import fpt.com.Product;
+
 public class BinaryStrategy implements fpt.com.SerializableStrategy {
+
+	@Override
+	public Product readObject() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void writeObject(Product obj) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/main/java/model/serialization/XStreamStrategy.java
+++ src/main/java/model/serialization/XStreamStrategy.java
@@ -1,3 +1,27 @@
+package model.serialization;
+
+import java.io.IOException;
+
+import fpt.com.Product;
+
 public class XStreamStrategy implements fpt.com.SerializableStrategy {
+
+	@Override
+	public Product readObject() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void writeObject(Product obj) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
