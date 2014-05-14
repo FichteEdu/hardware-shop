071343cee313d69f5d6bb7ff92351c996b152dec
FichteFoll <fichtefoll2@googlemail.com>
2014-05-15 00:55:03 +0200

    Implement load and safe logic in shop controller
    Also remove useless getter and setter for private strat.

--- src/main/java/shop/ControllerShop.java
+++ src/main/java/shop/ControllerShop.java
@@ -2,16 +2,23 @@ package shop;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+import model.serialization.BinaryStrategy;
+import model.serialization.XMLStrategy;
+import fpt.com.Product;
+import fpt.com.SerializableStrategy;
 
 
 public class ControllerShop implements ActionListener {
 
 	private ModelShop	model;
 	private ViewShop	view;
-	private byte		strat;		//0=bin, 1=beans, 2=xstream
+	private byte		strat;	// 0=bin, 1=beans, 2=xstream
 
 	public ControllerShop() {
 	}
@@ -22,7 +29,7 @@ public class ControllerShop implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
 		if (e.getSource() instanceof JButton) {
 			JButton btn = (JButton) e.getSource();
 
@@ -39,41 +46,80 @@ public class ControllerShop implements ActionListener {
 		} else if (e.getSource() instanceof JMenuItem) {
 			JMenuItem itm = (JMenuItem) e.getSource();
 			switch (itm.getText()) {
-				case "Bin": setStrat((byte) 0);
+				case "Bin":
+					strat = (byte) 0;
 					break;
-				case "beans": setStrat((byte) 1);
+				case "beans":
+					strat = (byte) 1;
 					break;
-				case "xstream": setStrat((byte) 2);
+				case "xstream":
+					strat = (byte) 2;
 					break;
-				case "save": save();
+				case "save":
+					save();
 					break;
-				case "load": load();
+				case "load":
+					load();
 					break;
-				default: //Comming
-					System.out.println("Unknown Action for button: " + itm.getText());				
+				default: // Comming
+					System.out.println("Unknown Action for button: " + itm.getText());
 			}
 		} else {
 			System.out.println("Error: Input of unknown source.");
 		}
-		
+
 	}
-	
-	public byte getStrat() {
-		return this.strat;
-	}
-	
-	public void setStrat(byte strat) {
-		this.strat = strat;
+
+	private SerializableStrategy getStrat() {
+		SerializableStrategy binaryStrat;
+		switch (strat) { // 0=bin, 1=beans, 2=xstream
+			case 0:
+				binaryStrat = new BinaryStrategy();
+				break;
+			case 1:
+				binaryStrat = new XMLStrategy();
+				break;
+			default:
+				binaryStrat = null; // TODO: XSteamStrategy();
+				break;
+		}
+
+		return binaryStrat;
 	}
 
 	private void load() {
-		// TODO Auto-generated method stub
-		
+		SerializableStrategy binaryStrat = getStrat();
+
+		try {
+			Product p;
+			while (true) {
+				p = binaryStrat.readObject();
+				if (p == null)
+					break;
+				else
+					model.add(p);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Error while loading from file.");
+		}
 	}
 
 	private void save() {
-		// TODO Auto-generated method stub
-		
+		SerializableStrategy binaryStrat = getStrat();
+
+		try {
+			for (Product p : model)
+				binaryStrat.writeObject(p);
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Error while saving to file.");
+		} finally {
+			try {
+				binaryStrat.close();
+			} catch (IOException e) {
+			}
+		}
 	}
 
 	/**
