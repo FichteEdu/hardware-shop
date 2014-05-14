80812d45548f7c61ed9523bff423e613dd7780bc
Robbilie <git@eneticum.de>
2014-05-14 23:44:40 +0200

    beans strat

--- src/main/java/model/serialization/XMLStrategy.java
+++ src/main/java/model/serialization/XMLStrategy.java
@@ -1,5 +1,9 @@
 package model.serialization;
 
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import fpt.com.Product;
@@ -8,14 +12,22 @@ public class XMLStrategy implements fpt.com.SerializableStrategy {
 
 	@Override
 	public Product readObject() throws IOException {
-		// TODO Auto-generated method stub
+		try (FileInputStream fi = new FileInputStream("products.xml"); XMLDecoder decoder = new XMLDecoder(fi)) {
+			return (Product) decoder.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public void writeObject(Product obj) throws IOException {
-		// TODO Auto-generated method stub
-		
+		try (FileOutputStream fo = new FileOutputStream("products.xml"); XMLEncoder encoder = new XMLEncoder(fo)) {
+			encoder.writeObject(obj);
+			encoder.flush();
+		} catch ( IOException e) {
+			e.printStackTrace() ;
+		}
 	}
 
 	@Override
