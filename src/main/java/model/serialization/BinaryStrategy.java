8b4eb0e42c398c79dfbb57a3b4f8ff85f87eb690
Osskyw <maxime.nederkorn@ymail.com>
2014-05-15 00:31:28 +0200

    Implementation of the Binary Strategy

--- src/main/java/model/serialization/BinaryStrategy.java
+++ src/main/java/model/serialization/BinaryStrategy.java
@@ -1,27 +1,61 @@
 package model.serialization;
 
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 import fpt.com.Product;
 
 public class BinaryStrategy implements fpt.com.SerializableStrategy {
+	
+	private FileInputStream fis;
+	private FileOutputStream fos;
+	private ObjectInputStream ois;
+	private ObjectOutputStream oos;
 
 	@Override
 	public Product readObject() throws IOException {
-		// TODO Auto-generated method stub
+		if (ois == null) {
+			try {
+				fis = new FileInputStream("products.ser");
+				ois = new ObjectInputStream(fis);
+			} catch (IOException e) {
+					e.printStackTrace();
+			}
+		}
+		try {
+			return (Product) ois.readObject();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public void writeObject(Product obj) throws IOException {
-		// TODO Auto-generated method stub
-		
+		if (fis == null) {
+			try {
+				fos = new FileOutputStream("products.ser");
+				oos = new ObjectOutputStream(fos);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try (FileOutputStream fos = new FileOutputStream("products.ser"); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+			oos.writeObject(obj); // write Object
+			oos.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
 	}
 
 	@Override
 	public void close() throws IOException {
-		// TODO Auto-generated method stub
-		
+		if (fis != null)
+			fis.close();
+		if (fos != null)
+			fos.close();		
 	}
-
 }
