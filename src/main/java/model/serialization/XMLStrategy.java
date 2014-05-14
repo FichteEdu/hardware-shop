8f2f0b3ef149f116e7f21c3908fd1cf8da6341d6
Robbilie <git@eneticum.de>
2014-05-14 23:58:30 +0200

    beans strat v2

--- src/main/java/model/serialization/XMLStrategy.java
+++ src/main/java/model/serialization/XMLStrategy.java
@@ -9,31 +9,49 @@ import java.io.IOException;
 import fpt.com.Product;
 
 public class XMLStrategy implements fpt.com.SerializableStrategy {
+	private XMLDecoder decoder;
+	private XMLEncoder encoder;
 
 	@Override
 	public Product readObject() throws IOException {
-		try (FileInputStream fi = new FileInputStream("products.xml"); XMLDecoder decoder = new XMLDecoder(fi)) {
-			return (Product) decoder.readObject();
-		} catch (IOException e) {
-			e.printStackTrace();
+		if (this.decoder == null) {
+			try (FileInputStream fi = new FileInputStream("products.xml"); XMLDecoder d = new XMLDecoder(fi)) {
+				this.decoder = d;
+				return (Product) decoder.readObject();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				return (Product) decoder.readObject();
+			} catch (ArrayIndexOutOfBoundsException e) {
+				return null;
+			}
 		}
 		return null;
 	}
 
 	@Override
 	public void writeObject(Product obj) throws IOException {
-		try (FileOutputStream fo = new FileOutputStream("products.xml"); XMLEncoder encoder = new XMLEncoder(fo)) {
-			encoder.writeObject(obj);
-			encoder.flush();
-		} catch ( IOException e) {
-			e.printStackTrace() ;
+		if (this.encoder == null) {
+			try (FileOutputStream fo = new FileOutputStream("products.xml"); XMLEncoder e = new XMLEncoder(fo)) {
+				this.encoder = e;
+				encoder.writeObject(obj);
+				encoder.flush();
+			} catch ( IOException e) {
+				e.printStackTrace() ;
+			}
+		} else {
+			this.encoder.writeObject(obj);
+			this.encoder.flush();
 		}
 	}
 
 	@Override
 	public void close() throws IOException {
 		// TODO Auto-generated method stub
-		
+		this.decoder.close();
+		this.encoder.close();
 	}
 
 }
