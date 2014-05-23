53382af70d24847485aaf9c8be16eaab440c12a3
Maik <Maik@134.91.44.135>
2014-05-23 11:37:50 +0200

    Generated Class OpenJPAStrategy.java

---
+++ src/main/java/model/serialization/OpenJPAStrategy.java
@@ -0,0 +1,28 @@
+package model.serialization;
+
+import java.io.IOException;
+
+import fpt.com.Product;
+import fpt.com.SerializableStrategy;
+
+public class OpenJPAStrategy implements SerializableStrategy {
+
+	@Override
+	public Product readObject() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void writeObject(Product obj) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
