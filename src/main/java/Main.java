7be1cf0cec74895d460089c7e4a20baa950f2aff
FichteFoll <fichtefoll2@googlemail.com>
2014-06-12 01:48:38 +0200

    Disable the Customer UI stuff for now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import shop.ControllerShop;
 import shop.ModelShop;
 import shop.ViewShop;
-//import customer.ModelCustomer;
+// import customer.ModelCustomer;
 import customer.ViewCustomer;
 
 
@@ -13,17 +13,25 @@ public class Main {
 		ControllerShop c = new ControllerShop();
 		c.link(m, v);
 
+		v.setVisible(true);
+
+		// Not needed for u3u4
+		// customer(m);
+	}
+
+	@SuppressWarnings("unused")
+	private static void customer(ModelShop m) {
+
 		// TODO: there is no ControllerCustomer yet, so do it quick and dirty
 		// The view should not know about its model at creation time, but this
 		// will be taken care of at a later point in time.
 		//
-//		ModelCustomer mc = new ModelCustomer();
+		// ModelCustomer mc = new ModelCustomer();
 		ViewCustomer vc = new ViewCustomer();
 		// link manually
 		vc.setModel(m);
 		m.addObserver(vc);
 
-		v.setVisible(true);
 		vc.setVisible(true);
 	}
 
