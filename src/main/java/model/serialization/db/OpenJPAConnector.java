30507bbec9d87df03e58027c6b0f8747d82b2097
FichteFoll <fichtefoll2@googlemail.com>
2014-06-12 14:02:22 +0200

    Make OpenJPA also work with a config file

--- src/main/java/model/serialization/db/OpenJPAConnector.java
+++ src/main/java/model/serialization/db/OpenJPAConnector.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+// import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 import org.apache.openjpa.persistence.OpenJPAPersistence;
